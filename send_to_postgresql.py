996d8cf8e623fafb129923a12e6a946a962a2690
n1k0din <nik726@gmail.com>
2021-08-12 12:14:40 +0700

    Add parameter - day

--- send_to_postgresql.py
+++ send_to_postgresql.py
@@ -4,14 +4,7 @@ from datetime import datetime
 import psycopg2
 
 
-def main():
-    today = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
-
-    filename = f'{today.strftime("%Y%m%d")}_buildings.json'
-
-    with open(filename) as f:
-        buildings = json.load(f)
-
+def buildings_to_pgsql(buildings, date):
     conn = psycopg2.connect('dbname=n1 user=postgres password=postgres')
     #
     cur = conn.cursor()
@@ -32,33 +25,48 @@ def main():
 
         flats = metadata['flats']
         for flat_id, flat_data in flats.items():
-            cur.execute(
-                """INSERT INTO flat_record (
-                        flat_id,
-                        record_date,
-                        building,
-                        url,
-                        area,
-                        apartment_floor,
-                        max_floor,
-                        material
-                    )
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-                """, (
+            sql = """INSERT INTO flat_record (
                     flat_id,
-                    today,
-                    building_id,
-                    flat_data['url'],
-                    flat_data['area'],
-                    flat_data['apartment_floor'],
-                    flat_data['max_floor'],
-                    flat_data['material'],
+                    record_date,
+                    building,
+                    url,
+                    area,
+                    apartment_floor,
+                    max_floor,
+                    material,
+                    price
                 )
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                ON CONFLICT(flat_id,record_date) DO UPDATE SET price = EXCLUDED.price
+            """
+
+            data = (
+                flat_id,
+                date,
+                building_id,
+                flat_data['url'],
+                flat_data['area'],
+                flat_data['apartment_floor'],
+                flat_data['max_floor'],
+                flat_data['material'],
+                flat_data['price'],
             )
+
+            cur.execute(sql, data)
 
     conn.commit()
     cur.close()
     conn.close()
+
+
+def main():
+    today = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
+    filename = f'{today.strftime("%Y%m%d")}_buildings.json'
+
+    with open(filename) as f:
+        buildings = json.load(f)
+
+    buildings_to_pgsql(buildings, today)
 
 
 if __name__ == '__main__':
